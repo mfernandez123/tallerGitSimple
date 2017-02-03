01478a4b1066992100faa87fdaed14cd25a691bd
Mariano Fernández Hernández <mfernandez@gfi.es>
2017-02-03 12:26:05 +0100

    enfatizar la admiracion del saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo!!");
     }
 
 }
